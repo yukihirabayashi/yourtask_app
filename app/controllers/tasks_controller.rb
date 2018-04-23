0fba06d7b032cc1b573fbea04a15c28272c73fc9
Yuki Hirabayashi <hira_y@nttpc.co.jp>
2018-04-23 10:54:01 +0000

    タスク一覧作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,24 @@
 class TasksController < ApplicationController
+
+  def index
+     @tasks = Task.all
+  end   
+
   def new
+      @task = Task.new
   end
   
   def create
+    Task.create(task_params)
+    redirect_to new_task_path
   end
-  
-  def index
+
+  def show
   end
+
+  private
   
+  def task_params
+    params.require(:task).permit(:name, :status, :content, :image, :request_user_id, :action_user_id, :deadline)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  
+  #belongs_to :user
+  
+    validates :name, presence: true
+    validates :content, presence: true
 end
