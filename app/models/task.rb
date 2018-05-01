c9d5e542a9dedb4b6072bff5eb7684ad76615712
Yuki Hirabayashi <hira_y@nttpc.co.jp>
2018-05-01 09:09:33 +0000

    バリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,9 +2,8 @@ class Task < ApplicationRecord
   
   belongs_to :user
   
-    
-    
     validates :content, presence: true
+    validates :content, length: [ maximum:100 ]  # 100文字以下
     
     mount_uploader :image, ImageUploader
     
