3efc340024d81b3718bb3b82873fdc494b99d000
Yuki Hirabayashi <hira_y@nttpc.co.jp>
2018-05-04 05:54:47 +0000

    ファイル不要記述削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,17 +3,7 @@ class Task < ApplicationRecord
   belongs_to :user
   
     validates :content, presence: true
-<<<<<<< HEAD
-<<<<<<< HEAD
     validates :content,    length: { maximum: 75 }      # 「75文字以下」
-=======
-    validates :content, length: [ maximum:100 ]  # 100文字以下
->>>>>>> afefe615c53ef2a43203df56cf08ceef46a51aeb
-=======
-    validates :content, length:  { maximum: 100 } # 100文字以下
->>>>>>> 7ed02c51621ca5efbbb80ef3f28784e939c1c1c2
-    
+
     mount_uploader :image, ImageUploader
-    
-   
 end
