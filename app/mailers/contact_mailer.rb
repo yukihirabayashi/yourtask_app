361c322dd3739773ec4efe29a865a097a2939c8f
Yuki Hirabayashi <hira_y@nttpc.co.jp>
2018-05-05 01:40:32 +0000

    バリデーション不具合対応中

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,8 +3,7 @@ class ContactMailer < ApplicationMailer
     def contact_mail(task)
 
       @task = task
-
-       mail to: User.all.find(@task.action_user_id).email, subject:"タスク登録完了メール"
+      mail to: User.all.find(@task.action_user_id).email, subject:"新たなタスクです！"
 
     end
 end
